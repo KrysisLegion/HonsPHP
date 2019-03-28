85794815dc38b5dc75ed9cf99cfee07ae852e139
korour203 <kristopher.orourke97@gmail.com>
2019-03-28 20:06:56 +0000

    db connection

--- PHP/database.php
+++ PHP/database.php
@@ -1,23 +1,40 @@
 <?php
 //open connection to database
-function openConnect(){
-  $dbhost = getenv("MYSQL_SERVICE_HOST");
-  $dbport = getenv("MYSQL_SERVICE_PORT");
-  $dbuser = getenv("databaseuser");
-  $dbpwd = getenv("databasepassword");
-  $dbname = getenv("databasename");
-  $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-  if ($connection->connect_errno) {
-      printf("Connect failed: %s\n", $mysqli->connect_error);
-      exit();
-  } else {
-      printf("Connected to the database");
-  }
-  $connection->close();
+
+
+
+
 
 function CreateAccount(){
+  $dbhost = getenv("MYSQL_SERVICE_HOST");
+  $dbport = getenv("MYSQL_SERVICE_PORT");
+  $dbuser = getenv("dbuser");
+  $dbpwd = getenv("dbpwd");
+  $dbname = getenv("dbname");
 
+  $dsn = "mysql:host=".$dbhost.";dbname=".$dbname.";charset=utf8mb4";
+    $options = [
+      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+      $pdo = new PDO($dsn, $dbuser, $dbpwd, $options);
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      exit('Database connection error has occured');
+    }
 
+    // this checks if the user trying to register already exists in the database
+    $stmt = $pdo->prepare("SELECT * FROM accs WHERE usr = ?");
+    $stmt->execute([$uname]);
+    $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = null;
+    if(!$arr){
+      return false;
+    }else{
+      return true;
+    }
 }
 
 function CheckLogin(){
