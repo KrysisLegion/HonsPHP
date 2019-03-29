e9126e857d74e4f40652d325a25ad6b3de48b7fa
korour203 <kristopher.orourke97@gmail.com>
2019-03-29 11:01:48 +0000

    new create account attempt #1

--- PHP/createAccount.php
+++ PHP/createAccount.php
@@ -13,7 +13,7 @@ if(isset($_POST['uname']) && isset($_POST['pword'])){
 }
 
 //open connection to database
-function checkUser(){
+function checkUser($uname){
 
 $dbhost = getenv("MYSQL_SERVICE_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_SERVICE_PORT");
@@ -21,49 +21,60 @@ $dbuser = getenv("root");
 $dbpwd = getenv("MYSQL_ROOT_PASSWORD");
 $dbname = getenv("dbname");
 
-  $connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
-  if (!$connection) {
-      printf("Connect failed:");
-  } else {
-      printf("Connected to the database");
-  }
+$dsn = "mysql:host=".$dbhost.":".$dbport.";dbname=".$dbname.";charset=utf8mb4";
+    $options = [
+      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+      $pdo = new PDO($dsn, $dbuser, $dbpwd, $options);
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      exit('Database connection error has occured');
+    }
 
-  $dbconnection = mysql_query($query);
-  while ($row = mysql_fetch_assoc($rs)){
-    echo $row['user_id']." ".$row['username']."\n";
-  }
-  mysql_close();
+    // this checks if the user trying to register already exists in the database
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$uname]);
+    $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = null;
+    if(!$arr){
+      return false;
+    }else{
+      return true;
+    }
 }
 
 //This inserts the created account into the database and hashes the password so it cannot be read if the database is breached
 function register($uname, $pwd){
-  $dbhost = getenv("localhost");
-  $dbport = getenv("MYSQL_SERVICE_PORT");
-  $dbuser = getenv("dbuser");
-  $dbpwd = getenv("dbpassword");
+  $dbhost = getenv("MYSQL_SERVICE_SERVICE_HOST");
+  $dbport = getenv("MYSQL_SERVICE_SERVICE_PORT");
+  $dbuser = getenv("root");
+  $dbpwd = getenv("MYSQL_ROOT_PASSWORD");
   $dbname = getenv("dbname");
 
-  $connection = mysql_connect($dbhost, $dbuser, $dbpwd, $dbname);
-  if (!$connection) {
-      printf("Connect failed:");
-  } else {
-      printf("Connected to the database");
-  }
+  $dsn = "mysql:host=".$dbhost.":".$dbport.";dbname=".$dbname.";charset=utf8mb4";
+      $options = [
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      ];
+      try {
+        $pdo = new PDO($dsn, $dbuser, $dbpwd, $options);
+      } catch (Exception $e) {
+        error_log($e->getMessage());
+        exit('Database connection error has occured');
+      }
 
-  $dbconnection = mysql_query($query);
-  while ($row = mysql_fetch_assoc($rs)){
-    echo $row['user_id']." ".$row['username']."\n";
-  }
-  mysql_close();
-  //
-  // $sql = "INSERT INTO users (username, password)
-  // VALUES ($uname, $password)";
-  //
-  // if ($conn->query($sql) === TRUE) {
-  //     echo "New record created successfully";
-  // } else {
-  //     echo "Error nooties: " . $sql . "<br>" . $conn->error;
-  // }
+      $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+
+    $options = [
+        'cost' => 12,
+    ];
+
+    $stmt->execute([$uname, password_hash($pwd, PASSWORD_BCRYPT, $options)]);
+    $_SESSION['usr'] = $uname;
 }
 
  ?>
